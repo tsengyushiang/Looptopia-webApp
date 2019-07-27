1bc768b069f2dfe3aba21d660f968ca08c60f701
tsengyushiang <tsengyushiang@gmail.com>
2019-07-27 22:09:54 +0800

    test Allow-Control

--- assets/script/NetWrokManger.ts
+++ assets/script/NetWrokManger.ts
@@ -6,24 +6,26 @@ export default class NetworkManger {
 
     static saveFile(dataString: string, callBack: Function) {
 
-    
-        axios.get('https://script.google.com/macros/s/AKfycbybBFcLXv_lQ-KNnxg_SUOWEIVHdmC__PTBqpB9Sg/exec',
-            {
-                headers: {"Access-Control-Allow-Origin": "*"},
-                params: {
-                    data: dataString
-                }
+
+        axios({
+            method: 'post',
+            url: 'https://script.google.com/macros/s/AKfycbwk1bNZccy5l6yYKtYd1VAcPuOCkyxAb69DEXHF7z0-2lDhYBDB/exec',
+            data: {
+                data: dataString,
+            },
+            headers: {
+                'Content-Type': 'text/plain;charset=utf-8',
             }
-        ).then(function (response) {
+        }).then(function (response) {
 
-            /*
+            
             let data = [];
-
+ 
             response.data.forEach(element => {
                 data.push(JSON.parse(element));
             });
-            */
-            callBack(response)
+            
+            callBack(data)
         }).catch(function (err) {
             console.log("Error : ", err);
         })
