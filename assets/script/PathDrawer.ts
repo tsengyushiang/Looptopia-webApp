b28ab4198b2e57a3527affc29445f8ff3afe02cc
tsengyushiang <tsengyushiang@gmail.com>
2019-07-28 01:33:42 +0800

    add recordScene replaySrpite

--- assets/script/PathDrawer.ts
+++ assets/script/PathDrawer.ts
@@ -1,8 +1,7 @@
 import PathRecorder from "./PathRecorder";
+import NetworkManger from "./NetWrokManger";
 
 const { ccclass, property } = cc._decorator;
-
-cc.Component.EventHandler
 
 @ccclass
 export default class PathDrawer extends cc.Component {
@@ -10,6 +9,11 @@ export default class PathDrawer extends cc.Component {
     graphics: cc.Graphics
     path = [];
 
+    @property
+    drawPath: boolean = false;
+
+    @property
+    loop: boolean = false;
 
     @property
     totalPlayTime: number = 10;
@@ -18,14 +22,21 @@ export default class PathDrawer extends cc.Component {
 
     onLoad() {
 
-        this.path = this.getComponent(PathRecorder).getPath();
+        //this.path = this.getComponent(PathRecorder).getPath();
+
+        let self = this;
+        NetworkManger.saveFile("", function (res) {
+            self.path = res[res.length - 1];
+        })
         this.graphics = this.getComponent(cc.Graphics);
     }
-
+    
     update(dt) {
 
+        if (this.path.length <= 0) return;
+
         let preindex_afterNormalize = this.currentTime / this.totalPlayTime * this.path.length;
-        let preindex =  Math.floor(preindex_afterNormalize)
+        let preindex = Math.floor(preindex_afterNormalize)
 
         this.currentTime += dt;
 
@@ -34,18 +45,23 @@ export default class PathDrawer extends cc.Component {
         let index = Math.floor(index_afterNormalize);
 
 
-        if (index > 0 && index < this.path.length) {
+        if (this.drawPath && index > 0 && index < this.path.length) {
 
             this.graphics.moveTo(
-                this.path[preindex].x + 960,
-                this.path[preindex].y + 540);
+                this.path[preindex].x,
+                this.path[preindex].y);
 
             this.graphics.lineTo(
-                this.path[index].x + 960,
-                this.path[index].y + 540);
+                this.path[index].x,
+                this.path[index].y);
 
             this.graphics.stroke();
 
+        }
+
+        if (this.loop && this.currentTime > this.totalPlayTime) {
+            this.graphics.clear();
+            this.currentTime = 0;
         }
 
     }
